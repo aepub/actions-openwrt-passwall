e34cd30a4b1b1842d30744b661338b7ee447c8cd
Guo <44573941+aepub@users.noreply.github.com>
2020-03-21 10:00:12 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
+./scripts/feeds update -a
+./scripts/feeds install -a
